52390d046661eb7ec4c738b3b78a9b4888c8e4e6
Sergey Konoplev <sn.konoplev@gmail.com>
2021-12-16 16:19:28 +0300

    fix cpu usage calculation

--- src/entity/cpu_usage.go
+++ src/entity/cpu_usage.go
@@ -1,9 +1,9 @@
 package entity
 
 type CpuUsage struct {
-	Count  int    `json:"count" example:"1637768672"`
-	Total  uint64 `json:"total" example:"1637768672"`
-	User   uint64 `json:"user" example:"1637768672"`
-	System uint64 `json:"system" example:"1637768672"`
-	Idle   uint64 `json:"idle" example:"1637768672"`
+	Count  int     `json:"count" example:"1637768672"`
+	Total  float64 `json:"total" example:"1637768672"`
+	User   float64 `json:"user" example:"1637768672"`
+	System float64 `json:"system" example:"1637768672"`
+	Idle   float64 `json:"idle" example:"1637768672"`
 }
--- src/queryhandler/cpu_usage_handler.go
+++ src/queryhandler/cpu_usage_handler.go
@@ -2,6 +2,7 @@ package queryhandler
 
 import (
 	"context"
+	"time"
 
 	"github.com/mackerelio/go-osstat/cpu"
 	"github.com/snkonoplev/file-manager/entity"
@@ -15,16 +16,22 @@ func NewCpuUsageHandler() *CpuUsageHandler {
 }
 
 func (h *CpuUsageHandler) Handle(context context.Context, q interface{}) (interface{}, error) {
-	cpu, err := cpu.Get()
+	before, err := cpu.Get()
+	if err != nil {
+		return nil, err
+	}
+
+	time.Sleep(time.Duration(1) * time.Second)
+	after, err := cpu.Get()
 	if err != nil {
 		return nil, err
 	}
 
 	return entity.CpuUsage{
-		Count:  cpu.CPUCount,
-		Total:  cpu.Total,
-		User:   cpu.User,
-		System: cpu.System,
-		Idle:   cpu.Idle,
+		Count:  after.CPUCount,
+		Total:  float64(after.Total - before.Total),
+		User:   float64(after.User - before.User),
+		System: float64(after.System - before.System),
+		Idle:   float64(after.Idle - before.Idle),
 	}, nil
 }
